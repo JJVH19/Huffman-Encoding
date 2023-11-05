1a3bef3fdfb722b5d095b061370680f2d5ffde49
JJVH19 <jjvelasquez@arizona.edu>
2023-11-04 17:59:26 -0700

    Added tree class, need hashtable of frequencies and priority queue

--- HuffmanMain.java
+++ HuffmanMain.java
@@ -1,2 +1,26 @@
 public class HuffmanMain {
+    static TreeCreator huffmanTree;
+
+    public static void main(String[] args){
+
+
+        //frequency data structure here
+
+        //Simulation to add things to the tree
+        int[] frequencySimulator = new int[]{17,28,37,49,59,68};
+        boolean firstRoot = true;
+        for(int i = 0; i < frequencySimulator.length; i++){
+            if(firstRoot){
+                huffmanTree = new TreeCreator(frequencySimulator[i],frequencySimulator[i+1]);
+                firstRoot = false;
+            } else{
+                huffmanTree = huffmanTree.addFrequency(huffmanTree.root, frequencySimulator[i]);
+            }
+        }
+
+
+
+
+
+    }
 }
