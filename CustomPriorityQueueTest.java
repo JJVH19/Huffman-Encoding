31dbc12f188b23c4a06b7407fbd25870dc2155cd
JJVH19 <jjvelasquez@arizona.edu>
2023-11-12 13:35:25 -0700

    fixed wrong arithmetic in test cases

--- CustomPriorityQueueTest.java
+++ CustomPriorityQueueTest.java
@@ -109,12 +109,53 @@ class CustomPriorityQueueTest {
         assertEquals(test3.getNode().getLetter(),'c');
         assertEquals(testQueue.size(),2);
 
-        nodeCreator finalTest = testQueue.poll();
-        assertEquals(finalTest.getNode().getFrequency(),33);
-        assertEquals(finalTest.getNode().getLeftChild().getFrequency(),10);
-        assertEquals(finalTest.getNode().getLeftChild().getLetter(),'d');
-        assertEquals(finalTest.getNode().getRightChild().getFrequency(),23);
-        assertEquals(finalTest.getNode().getRightChild().getLetter(),'q');
+        nodeCreator test4 = testQueue.poll();
+        assertEquals(test4.getNode().getFrequency(),33);
+        assertEquals(test4.getNode().getLeftChild().getFrequency(),10);
+        assertEquals(test4.getNode().getLeftChild().getLetter(),'d');
+        assertEquals(test4.getNode().getRightChild().getFrequency(),23);
+        assertEquals(test4.getNode().getRightChild().getLetter(),'q');
         assertEquals(testQueue.size(),1);
+
+        nodeCreator secondConnection = new nodeCreator((test3.getNode().getFrequency() + test4.getNode().getFrequency()));
+        assertEquals(secondConnection.getNode().getFrequency(),56);
+
+        secondConnection.getNode().setLeftChild(test3.getNode());
+        assertEquals(secondConnection.getNode().getLeftChild().getFrequency(),23);
+        assertEquals(secondConnection.getNode().getLeftChild().getLetter(),'c');
+
+        secondConnection.getNode().setRightChild(test4.getNode());
+        assertEquals(secondConnection.getNode().getRightChild().getFrequency(),33);
+
+        testQueue.addandMove(secondConnection);
+        assertEquals(testQueue.size(),2);
+
+        nodeCreator finalTest1 = testQueue.poll();
+        assertEquals(testQueue.size(),1);
+        assertEquals(finalTest1.getNode().getFrequency(),56);
+        assertEquals(finalTest1.getNode().getLeftChild().getFrequency(),23);
+        assertEquals(finalTest1.getNode().getLeftChild().getLetter(),'c');
+        assertEquals(finalTest1.getNode().getRightChild().getFrequency(),33);
+
+        nodeCreator finalTest2 = testQueue.poll();
+        assertEquals(testQueue.size(),0);
+        assertTrue(testQueue.isEmpty());
+        assertEquals(finalTest2.getNode().getFrequency(),85);
+        assertEquals(finalTest2.getNode().getLetter(),'s');
+
+        nodeCreator root = new nodeCreator(finalTest1.getNode().getFrequency() + finalTest2.getNode().getFrequency());
+        assertEquals(root.getNode().getFrequency(),141);
+
+        root.getNode().setLeftChild(finalTest1.getNode());
+        assertEquals(root.getNode().getLeftChild().getFrequency(),56);
+
+        root.getNode().setRightChild(finalTest2.getNode());
+        assertEquals(root.getNode().getRightChild().getFrequency(),85);
+        assertEquals(root.getNode().getRightChild().getLetter(),'s');
+
+//        testQueue.addandMove(root);
+//        assertEquals(testQueue.size(),1);
+//        assertFalse(testQueue.isEmpty());
+
     }
 }
