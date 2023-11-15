40025d8dcd716a7cd2972b7cf54be2c9b44838e4
Jose Velasquez <jjvelasquez@arizona.edu>
2023-11-15 16:30:04 -0700

    pending encode

--- HuffmanMain.java
+++ HuffmanMain.java
@@ -1,22 +1,17 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class HuffmanMain {
-
     public static void main(String[] args){
-
+        CustomMap<Character, Integer> hashTable = new CustomMap<>(256);
+        CustomPriorityQueue<nodeCreator> huffmanTreeQueue = new CustomPriorityQueue<>(Comparator.comparingInt(o -> o.getNode().getFrequency()));
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the phrase to encode:\n");
         String userInput = scanner.nextLine();
 
-        CustomMap<Character, Integer> hashTable = new CustomMap<>(256);
-        CustomPriorityQueue<nodeCreator> huffmanTreeQueue = new CustomPriorityQueue<>(Comparator.comparingInt(o -> o.getNode().getFrequency()));
-
-
         for(int character = 0; character < userInput.toCharArray().length; character++){
-            System.out.println(userInput.toCharArray()[character]);
             char currentChar = userInput.toCharArray()[character];
             if(hashTable.get(userInput.toCharArray()[character]) != null){
                 hashTable.put(currentChar,hashTable.get(currentChar)+1);
@@ -24,7 +19,6 @@ public class HuffmanMain {
                 hashTable.put(currentChar,1);
             }
         }
-        //userInput.toString().toCharArray();
 
         List<Character> frequencies = hashTable.keys();
 
@@ -41,9 +35,29 @@ public class HuffmanMain {
             newRoot.getNode().setRightChild(node2.getNode());
             huffmanTreeQueue.addandMove(newRoot);
         }
+        nodeCreator huffmanTree = huffmanTreeQueue.poll();
 
-        System.out.println("Done");
+//        String encoded = "";
+//        for(int character = 0; character < userInput.toCharArray().length; character++){
+//            char currentChar = userInput.toCharArray()[character];
+//            encoded+=encode(huffmanTree.getNode(),currentChar);
+//        }
+//        System.out.println(encoded);
         //Priority queue insertion here
 
     }
+
+//    public static String encode(nodeCreator.HuffmanNode node, char currentChar){
+//        if(node.getLeftChild() == null && node.getRightChild() == null && node.getLetter() == currentChar){
+//            return "";
+//        }
+//        if(node.getLeftChild() != null){
+//            return "0" + encode(node.getLeftChild(),currentChar);
+//        }
+//        if(node.getRightChild() != null){
+//            return "1" + encode(node.getRightChild(),currentChar);
+//        }
+//
+//        return null;
+//    }
 }
