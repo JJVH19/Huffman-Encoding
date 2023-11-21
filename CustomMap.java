cfa6abc48e15c816135f99060c1974fd9b94b4e5
bryanlethestudentatuofa <125412706+bryanlethestudentatuofa@users.noreply.github.com>
2023-11-21 13:10:07 -0700

    Update CustomMap.java

--- CustomMap.java
+++ CustomMap.java
@@ -1,7 +1,14 @@
+/*
+ * This map supports adding, retrieving, and listing key-value based
+ * on the provided keys and values. It uses hash code of the keys to 
+ * distribute them among different buckets and each bucket is a linked list
+ * to handle collisions
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomMap<K, V> {
+    // Entry class represents key-value pairs in the map
     private static class Entry<K, V> {
         K key;
         V value;
@@ -13,19 +20,24 @@ public class CustomMap<K, V> {
         }
     }
 
-    private List<Entry<K, V>> bucketArray;
-    private int capacity;
-    private int size;
+    private List<Entry<K, V>> bucketArray;  // Array of linked lists (buckets) to store key-value pairs
+    private int capacity;                   // Capacity of the map (number of buckets)
+    private int size;                       // Number of key-value pairs in the map
 
+    // Constructor to initialize the map with a specified capacity
     public CustomMap(int capacity) {
         this.capacity = capacity;
         this.bucketArray = new ArrayList<>(capacity);
+
+        // Initialize each bucket with null
         for (int i = 0; i < capacity; i++) {
             bucketArray.add(null);
         }
-        this.size = 0;
+
+        this.size = 0;  // Initial size is zero
     }
 
+    // Add or update a key-value pair in the map
     public void put(K key, V value) {
         int bucketIndex = getBucketIndex(key);
         Entry<K, V> head = bucketArray.get(bucketIndex);
@@ -39,7 +51,7 @@ public class CustomMap<K, V> {
             head = head.next;
         }
 
-        // Key doesn't exist, add a new entry
+        // Key doesn't exist, add a new entry at the beginning of the bucket's linked list
         size++;
         head = bucketArray.get(bucketIndex);
         Entry<K, V> newEntry = new Entry<>(key, value);
@@ -47,10 +59,12 @@ public class CustomMap<K, V> {
         bucketArray.set(bucketIndex, newEntry);
     }
 
+    // Retrieve the value associated with a given key
     public V get(K key) {
         int bucketIndex = getBucketIndex(key);
         Entry<K, V> head = bucketArray.get(bucketIndex);
 
+        // Traverse the linked list in the bucket to find the key
         while (head != null) {
             if (head.key.equals(key)) {
                 return head.value;
@@ -61,6 +75,7 @@ public class CustomMap<K, V> {
         return null; // Key not found
     }
 
+    // Get a list of all keys in the map
     public List<K> keys() {
         List<K> keysList = new ArrayList<>();
         for (Entry<K, V> entry : bucketArray) {
@@ -73,10 +88,12 @@ public class CustomMap<K, V> {
         return keysList;
     }
 
+    // Get the number of key-value pairs in the map
     public int size() {
         return size;
     }
 
+    // Private method to calculate the bucket index for a given key
     private int getBucketIndex(K key) {
         int hashCode = key.hashCode();
         return hashCode % capacity;
