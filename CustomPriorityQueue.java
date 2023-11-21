2356c76db9ae5b4215d79e1d071cf56ed5c8f98a
bryanlethestudentatuofa <125412706+bryanlethestudentatuofa@users.noreply.github.com>
2023-11-21 13:09:39 -0700

    Update CustomPriorityQueue.java

--- CustomPriorityQueue.java
+++ CustomPriorityQueue.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
+/*
+ * This priority queue is implemented as binary heap as a complete binary tree
+ * where the value of each node is less than or equal to values of its children according
+ * to the comparator. This way it makes sure the highest priority is always at the root of the heap.
+ */
 public class CustomPriorityQueue<T> {
-    private List<T> data;
-    private Comparator<T> comparator;
+    private List<T> data;          // List to store the elements of the priority queue
+    private Comparator<T> comparator;  // Comparator for defining the priority order
 
+    // Constructor to initialize the priority queue with a comparator
     public CustomPriorityQueue(Comparator<T> comparator) {
         this.comparator = comparator;
         this.data = new ArrayList<>();
     }
 
+    // Add an item to the priority queue and adjust its position to maintain the heap property
     public void addandMove(T item) {
         data.add(item);
         siftUp(data.size() - 1);
     }
 
+    // Remove and return the element with the highest priority (root of the heap)
     public T poll() {
         if (isEmpty()) {
             return null;
@@ -33,15 +40,17 @@ public class CustomPriorityQueue<T> {
         return result;
     }
 
+    // Check if the priority queue is empty
     public boolean isEmpty() {
         return data.isEmpty();
     }
     
+    // Return the number of elements in the priority queue
     public int size() {
         return data.size();
     }
 
-
+    // Restore heap property by moving the element up the tree
     private void siftUp(int index) {
         T item = data.get(index);
 
@@ -60,6 +69,7 @@ public class CustomPriorityQueue<T> {
         data.set(index, item);
     }
 
+    // Restore heap property by moving the element down the tree
     private void siftDown(int index) {
         int size = data.size();
         T item = data.get(index);
